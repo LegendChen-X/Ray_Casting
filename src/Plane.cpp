05820a4e0192335c8f7e3253ee76e5b6f4bc1740
Xiang <chenxiang00825@gmail.com>
2020-05-16 12:54:09 -0400

    minor fix

--- src/Plane.cpp
+++ src/Plane.cpp
@@ -4,7 +4,6 @@
 bool Plane::intersect(
   const Ray & ray, const double min_t, double & t, Eigen::Vector3d & n) const
 {
-    
     if(ray.direction.dot(normal) == 0) return false;
     
     double t_buff = -(normal.dot(ray.origin - point)) / normal.dot(ray.direction);
